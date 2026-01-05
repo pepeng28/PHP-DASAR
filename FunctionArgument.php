0e81697504fa4e3cc1f8a64c2e11d78ad858ae40
pepeng28 <kangmunir28@gmail.com>
2026-01-06 00:50:47 +0700

    belajar

--- FunctionArgument.php
+++ FunctionArgument.php
@@ -18,3 +18,18 @@ sum(100 , 100);
 sum("100" , "100");
 sum(true , false);
 
+
+function sumAll(...$values) {
+
+  $total = 0;
+  foreach ($values as $value) {
+    $total += $value;
+  }
+
+  echo "total " . implode("," , $values) . " = $total" . PHP_EOL;
+}
+
+$values = [1, 2, 3, 4, 5];
+
+sumAll(1, 2, 3, 4, 5);
+sumAll(...$values);
