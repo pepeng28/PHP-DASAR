8d55477297c75d1c986c85bd793e86f886455303
pepeng28 <kangmunir28@gmail.com>
2026-01-05 22:55:17 +0700

    belajar

--- FunctionArgument.php
+++ FunctionArgument.php
@@ -1,9 +1,20 @@
 <?php
 
-function heyHello($firsName , $lastName = "") {
-  echo "hey hello $firsName $lastName" . PHP_EOL;
+function heyHello($firstName , $lastName = "") {
+  echo "hey hello $firstName $lastName" . PHP_EOL;
 }
 
 heyHello("el");
 heyHello("budi");
 heyHello("akmal" , "samsudin");
+
+
+function sum(int $first , int $last){
+  $total = $first + $last;
+  echo "total $first + $last = $total" .PHP_EOL;
+}
+
+sum(100 , 100);
+sum("100" , "100");
+sum(true , false);
+
