6646b733115414975b0d19676f68315fe499079e
pepeng28 <kangmunir28@gmail.com>
2026-01-25 08:54:38 +0700

    belajar bareng

---
+++ StringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join("," , [10 , 11 , 12 , 13 , 14 , 15]));
+var_dump(explode(" " , "ell belajar coding"));
+var_dump(strtolower("ELL BELAJAR CODING"));
+var_dump(strtoupper("ell belajar coding"));
+var_dump(trim("        ell          coding        "));
+var_dump(substr("ell belajar coding" , 0 , 3));
